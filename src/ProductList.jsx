42f634c0d5841273fd95fa163d31bed213f3585c
alishersv <alishersv@gmail.com>
2025-01-03 02:33:59 -0500

    another try

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,13 +6,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    const handleAddToCart = (product) => {
-        dispatch(addItem(product));
-        setAddedToCart((prevState)=>({
-            ...prevState,
-            [product.name]:true,
-        }));
-    };
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -254,7 +248,16 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
-    return (
+
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+     }));
+  };
+
+  return (
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
